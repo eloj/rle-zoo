9da61cacbee729e7b73f4bda8d8279dc45be1d5d
Eddy Jansson <eddy@klopper.net>
2022-02-06 22:53:12 +0100

    Robustness pass on rle_pcx.h

--- rle_pcx.h
+++ rle_pcx.h
@@ -10,18 +10,19 @@ extern "C" {
 
 #include <stdint.h>
 #include <stddef.h>
+#include <sys/types.h> // ssize_t
 
-size_t pcx_compress(const uint8_t *src, size_t slen, uint8_t *dest, size_t dlen);
-size_t pcx_decompress(const uint8_t *src, size_t slen, uint8_t *dest, size_t dlen);
+ssize_t pcx_compress(const uint8_t *src, size_t slen, uint8_t *dest, size_t dlen);
+ssize_t pcx_decompress(const uint8_t *src, size_t slen, uint8_t *dest, size_t dlen);
 
 #ifdef RLE_ZOO_PCX_IMPLEMENTATION
 #include <assert.h>
 
-size_t pcx_compress(const uint8_t *src, size_t slen, uint8_t *dest, size_t dlen) {
+ssize_t pcx_compress(const uint8_t *src, size_t slen, uint8_t *dest, size_t dlen) {
 	size_t rp = 0;
 	size_t wp = 0;
 
-	while (rp < slen && (wp < dlen || dest == NULL)) {
+	while (rp < slen) {
 		size_t cnt = 0;
 		// size_t cnt = rle_count_rep(src + rp, slen - rp, 63);
 		do { ++cnt; } while ((rp + cnt < slen) && (cnt < 63) && (src[rp + cnt - 1] == src[rp + cnt]));
@@ -29,18 +30,25 @@ size_t pcx_compress(const uint8_t *src, size_t slen, uint8_t *dest, size_t dlen)
 		// Output REP, also include any bytes that can't be encoded as a LIT.
 		if (cnt > 1 || ((src[rp] & 0xC0) == 0xC0)) { // or >= 192
 			assert(cnt <= 63);
-			if (dest && dlen > 0) {
-				dest[wp] = 0xC0 | cnt;
-				if (wp + 1 < dlen)
-					dest[wp + 1] = src[rp];
+			if (dest) {
+				if (wp + 1 < dlen) {
+					dest[wp+0] = 0xC0 | cnt;
+					dest[wp+1] = src[rp];
+				} else {
+					return -(rp+1); // Destination buffer too small
+				}
 			}
 			wp += 2;
 			rp += cnt;
 		} else {
 			// Output LIT.
 			// PERF: Again, this is probably suboptimal, and also results in encoding runs of 2 LITs as REP, which differs from IM encoder.
-			if (dest && dlen > 0) {
-				dest[wp] = src[rp];
+			if (dest) {
+				if (wp < dlen) {
+					dest[wp] = src[rp];
+				} else {
+					return -(rp+1);
+				}
 			}
 			++rp;
 			++wp;
@@ -51,25 +59,32 @@ size_t pcx_compress(const uint8_t *src, size_t slen, uint8_t *dest, size_t dlen)
 	return wp;
 }
 
-size_t pcx_decompress(const uint8_t *src, size_t slen, uint8_t *dest, size_t dlen) {
-	const uint8_t *send = src + slen;
+ssize_t pcx_decompress(const uint8_t *src, size_t slen, uint8_t *dest, size_t dlen) {
 	size_t wp = 0;
-	while (src < send && (wp < dlen || dest == NULL)) {
+	size_t rp = 0;
+	while (rp < slen) {
 		uint8_t cnt = 1;
-		uint8_t b = *src++;
+		uint8_t b = src[rp++];
 
 		// REP
 		if ((b & 0xC0) == 0xC0) {
+			if (!(rp < slen)) {
+				return -(rp + 1);
+			}
 			cnt = b & 0x3F;
-			b = (src < send) ? *src++ : 0; // Guard from OOB read
+			b = src[rp++];
 		}
 		if (dest) {
-			for (int i = 0 ; i < cnt && wp + i < dlen ; ++i)
-				dest[wp + i] = b;
+			if (wp + cnt <= dlen) {
+				for (int i = 0 ; i < cnt ; ++i)
+					dest[wp + i] = b;
+			} else {
+				return -(rp + 1);
+			}
 		}
 		wp += cnt;
 	}
-	assert(src == send);
+	assert(rp == slen);
 	assert((dest == NULL) || (wp <= dlen));
 	return wp;
 }
